daa8e363cb9dcd40f10a7c29e5cce3c22ce0a04e
Xaad <Xaad@users.noreply.github.com>
2019-08-26 17:36:43 -0600

    8/26/2019
    Project Initialization

---
+++ TataruBot/TataruBot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.WebSocket;
+
+namespace TataruBot
+{
+    class TataruBot
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args) => 
+            new TataruBot().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync() {
+            Console.WriteLine("Running Async");
+
+            var _configs = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_configs);
+            string file_path = Path.Combine(Environment.CurrentDirectory, "CompletelySecure.txt");
+            string token = File.ReadAllText(file_path);
+            Console.WriteLine(token);
+
+            // Events:
+            _client.Connected += Ready;
+
+
+            // Connect:
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Prevent connection from closing
+            await Task.Delay(-1);
+        }
+
+        public Task Ready() {
+            Console.Write("Connected");
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ TestBot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace TataruBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private DiscordSocketConfig _configs;
+        public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	    public async Task MainAsync()
+	    {
+            Console.WriteLine("Loading");
+
+            _configs = new DiscordSocketConfig();
+            _configs.MessageCacheSize = 1000;
+
+            _client = new DiscordSocketClient(_configs);
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+            _client.MessageUpdated += MessageUpdated;
+
+            await _client.LoginAsync(TokenType.Bot, "");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);   
+	    }
+
+        public async Task MessageReceived(SocketMessage msg){
+            if (msg.Content == "Test") {
+                string text = "Recieved";
+                await msg.Channel.SendMessageAsync(text);
+            }
+            if (msg.Content == "LogOff") {
+                await _client.LogoutAsync();
+            }
+        }
+
+        public async Task MessageUpdated(Cacheable<IMessage, UInt64> old, SocketMessage msg, 
+                                         ISocketMessageChannel chan)
+        {
+            string old_message;
+            Console.Write(old.HasValue);
+            if (old.HasValue) 
+            {
+                old_message = old.Value.Content;
+            } else
+            {
+                old_message = "Not Found";
+            }
+            await chan.SendMessageAsync(old_message);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
